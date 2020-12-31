74ea9ec136c2ee1c58d39aed8457f0222a794383
Morgan <morgan.espindola@gmail.com>
2020-12-31 15:09:58 -0300

    Module 04 - Class 08
    English = Creating Header (1 - Structure and logo)
    Portuguese = Criando o Header (1 - estrutura e logo)

--- module_04/src/components/partials/Header/index.js
+++ module_04/src/components/partials/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './index.css'
 
 function Header() {
 
     return <>
-        <div className='HeaderArea'>
-            Header
+        <div className='header-area'>
+            <div className='container'>
+                <div className='logo'>
+                    <Link to='/'>
+                        <span className='logo-1'>O</span>
+                        <span className='logo-2'>L</span>
+                        <span className='logo-3'>Z</span>
+                    </Link>
+                </div>
+            </div>
         </div>
     </>
 }
