8e7dd45f02e8e20706fd1805c9b77b597983b5f2
Morgan <morgan.espindola@gmail.com>
2021-01-03 13:55:37 -0300

    Module 04 - Class 20
    Home (part 1: searchBox)

--- module_04/src/pages/Home/index.js
+++ module_04/src/pages/Home/index.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { PageContainer } from '../../components/MainComponents.js';
+import { useApi } from '../../helpers/OlzAPI';
+import { PageArea, SearchArea } from './styled.js';
 
 function Home() {
+
+    const api = useApi();
+
+    const [stateLoc, setStateLoc] = useState('')
+    const [stateList, setStateList] = useState([])
+
+    useEffect(() => {
+        const getStates = async () => {
+            const slist = await api.getStates();
+            setStateList(slist);
+        }
+        getStates();
+    }, [])
+
     return <>
-        <h3>Home Page</h3>
-        <Link to='/about'>About</Link>
+        <SearchArea>
+            <PageContainer>
+                <div className="search-box">
+                    <form method="GET" action="/ads">
+                        <input type="text" name="q" placeholder="What do you need?"></input>
+                        <select value={stateLoc} onChange={(e) => setStateLoc(e.target.value)} required>
+                            <option></option>
+                            {stateList.map((state, index) =>
+                                <option key={index} value={state._id}>{state.name}</option>
+                            )}
+                        </select>
+                        <button>Search</button>
+                    </form>
+                </div>
+                <div className="category-list"></div>
+            </PageContainer>
+        </SearchArea>
+        <PageContainer>
+            <PageArea>
+                ...
+            </PageArea>
+        </PageContainer>
     </>
 }
 
