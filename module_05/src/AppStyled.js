bf365ddc73383b797471840b97fc7d26f01aaa20
Morgan <morgan.espindola@gmail.com>
2021-01-26 22:27:42 -0300

    Module 05 - Class 03 (02 hasn't code)
    Structure (part 1)
    
    (Class 02 was just explanation about the project)

---
+++ module_05/src/AppStyled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    background-color: #FF0000;
+`
+
+export const Menu = styled.div`
+    display: flex;
+    background-color: #00FF00;
+    width: 80px;
+
+`
+
+export const PageBody = styled.div`
+    display: flex;
+    background-color: #0000FF;
+    flex: 1;
+`
