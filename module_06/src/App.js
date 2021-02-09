efb309b4c6eb3935dee47051f897c7824c894b20
Morgan <morgan.espindola@gmail.com>
2021-02-09 10:14:52 -0300

    Module 06 - Class 03
    Sidebar Header
    * Structure and Stylization

--- module_06/src/App.js
+++ module_06/src/App.js
@@ -1,14 +1,36 @@
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React from 'react';
 import './App.css';
 
 export default () => {
-  return (
-    <div className="app-window">
-      <div className="sidebar">
-        sidebar area
-      </div>
-      <div className="content-area">
-        content-area area
-      </div>
-    </div>
-  );
+    return (
+        <div className="app-window">
+            <div className="sidebar">
+                <header>
+                    <img className="header-avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt=""></img>
+                    <div className="header-buttons">
+                        <div className="header-button">
+                            <DonutLargeIcon style={{ color: '#919191' }} />
+                            <ChatIcon style={{ color: '#919191' }} />
+                            <MoreVertIcon style={{ color: '#919191' }} />
+                        </div>
+                    </div>
+                </header>
+
+                <div className="search">
+                    search
+                </div>
+
+                <div className="chatList">
+                    chatlist
+                </div>
+            </div>
+
+            <div className="content-area">
+                content area
+            </div>
+        </div>
+    );
 }
